0294262ee003e789f76694a1299917185b91d348
SergeyReizman <sergeytlv1971@gmail.com>
2025-02-05 12:01:48 +0200

    crystal snowfall

--- calendar.js
+++ calendar.js
@@ -48,7 +48,8 @@ const applyTheme = (theme) => {
     'serene-shores', 'blue-ice', 'cherry-garden', 'deep-forest',
     'orange-country', 'neon-nights', 'desert-mirage', 'lavender-bliss',
     'spring-meadow', 'galactic-glow', 'golden-horizon', 'cyber-pulse', 'northern-nights', 
-    'arctic-glow', 'new-ai', 'steel-shadows', 'tropical-sunset', 'exotic-island', 'pink-lake'
+    'arctic-glow', 'new-ai', 'steel-shadows', 'tropical-sunset', 'exotic-island', 
+    'pink-lake', 'crystal-snowfall'
   );
 
   body.classList.add(theme);
