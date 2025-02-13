58adb2693ca95c459f823415aa0b1d26b65834b4
SergeyReizman <sergeytlv1971@gmail.com>
2025-02-13 21:19:58 +0200

    tm

--- calendar.js
+++ calendar.js
@@ -54,7 +54,7 @@ const applyTheme = (theme) => {
     'black-hacker', 'cyber-pulse', 'dragon-flame', 'solar-flare', 'pure-light',
     'shaolin-monastery', 'black-coffee', 'forgotten-dreams', 'wild-wonders',
     'secret-night', 'candy-world', 'cracked-ice', 'fir-balsam', 'sea-diamonds',
-    'antarctic-snowdrifts', 'silicon-valley'
+    'antarctic-snowdrifts', 'silicon-valley', 'time-machine'
   );
 
   body.classList.add(theme);
