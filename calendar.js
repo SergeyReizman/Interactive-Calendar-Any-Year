190e0a39288c8887cdde5c634d4882380fea6072
SergeyReizman <sergeytlv1971@gmail.com>
2025-02-14 13:54:58 +0200

    0031

--- calendar.js
+++ calendar.js
@@ -55,7 +55,7 @@ const applyTheme = (theme) => {
     'shaolin-monastery', 'black-coffee', 'forgotten-dreams', 'wild-wonders',
     'secret-night', 'candy-world', 'cracked-ice', 'fir-balsam', 'sea-diamonds',
     'antarctic-snowdrifts', 'silicon-valley', 'time-machine', 'magellanic-penguin', 
-    'white-shark'
+    'white-shark', 'blue-whale'
   );
 
   body.classList.add(theme);
