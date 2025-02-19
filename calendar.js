567a5c636b0c7ea1e81346f5f9dcaf287dcbe109
SergeyReizman <sergeytlv1971@gmail.com>
2025-02-19 21:22:19 +0200

    0158

--- calendar.js
+++ calendar.js
@@ -58,7 +58,7 @@ const applyTheme = (theme) => {
     'white-shark', 'blue-whale', 'polar-bear', 'mountain-lake', 'flamingo-sky',
     'cacao-plantation', 'green-coffee', 'sea-lions', 'midnight-train',
     'rainforest-jungle', 'great-wall','viking-helmet', 'bamboo-flute', 
-    'golden-gate', 'red-lake'
+    'golden-gate', 'red-lake','kilimanjaro-outskirts'
   );
 
   body.classList.add(theme);
