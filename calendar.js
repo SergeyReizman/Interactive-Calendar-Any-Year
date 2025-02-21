4ece76c199af3c7b1e75595de224322b365e42a5
SergeyReizman <sergeytlv1971@gmail.com>
2025-02-21 11:00:55 +0200

    bp theme

--- calendar.js
+++ calendar.js
@@ -59,7 +59,7 @@ const applyTheme = (theme) => {
     'cacao-plantation', 'green-coffee', 'sea-lions', 'midnight-train',
     'rainforest-jungle', 'great-wall','viking-helmet', 'bamboo-flute', 
     'golden-gate', 'red-lake','kilimanjaro-outskirts', 'rainbow-dream',
-    'blue-planet', 'rising-sun'
+    'blue-planet', 'rising-sun', 'best-practice'
   );
 
   body.classList.add(theme);
