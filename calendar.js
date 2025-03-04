b0f1292f47d5f62a26779845bf38b5c0aa7bf750
SergeyReizman <sergeytlv1971@gmail.com>
2025-03-04 17:26:53 +0200

    mp

--- calendar.js
+++ calendar.js
@@ -65,7 +65,7 @@ const applyTheme = (theme) => {
     'blue-bird', 'aloe-vera', 'port-victoria', 'alpine-landscape', 'mount-fuji',
     'niagara-falls', 'heavy-rain', 'the-last-mohican', 'finish-line', 'porcupine-quill',
     'genghis-khan-arrow', 'okinawa-breeze', 'weekend-downtime', 'dartagnans-rapier',
-    'maple-leaf-fall', 'lions-mane', 'cafe-de-colombia'
+    'maple-leaf-fall', 'lions-mane', 'cafe-de-colombia', 'moonlit-path'
   );
 
   body.classList.add(theme);
