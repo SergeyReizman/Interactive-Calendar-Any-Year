637e91ef7779a01abc0e05ad7052833ccd3650ce
SergeyReizman <sergeytlv1971@gmail.com>
2025-03-16 13:47:45 +0200

    gi

--- calendar.js
+++ calendar.js
@@ -71,7 +71,7 @@ const applyTheme = (theme) => {
     'crystal-stream', 'black-velvet', 'adriatic-sea', 'sea-star', 'royal-caribbean', 
     'abandoned-castle', 'red-squirrel', 'shift-in-perception', 'migratory-birds', 
     'cardinals-guards', 'sword-of-fate', 'cinnamon-street', 'lemongrass-sky', 
-    'maple-syrup', 'robin-good'
+    'maple-syrup', 'robin-good', 'greenland-icebergs'
   );
 
   body.classList.add(theme);
