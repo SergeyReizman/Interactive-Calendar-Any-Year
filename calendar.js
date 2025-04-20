c3ef6982c7f701c222fa45bd5590b22cb5f9648a
SergeyReizman <sergeytlv1971@gmail.com>
2025-04-20 22:09:52 +0300

    00

--- calendar.js
+++ calendar.js
@@ -83,7 +83,7 @@ const applyTheme = (theme) => {
     'iceland-volcanoes' , 
     'imaginary-world', 'inspiration-line', 'in-the-dusk', 'intelligence-quotient', 'into-the-night', 
     'invincible-zone', 'japan-emperor','kilimanjaro-outskirts', 'lavender-bliss', 
-    'lemongrass-sky', 'life-quality', 'lions-mane', 'logical-intelligence',
+    'lemongrass-sky', 'life-quality', 'limited-edition', 'lions-mane', 'logical-intelligence',
     'love-station', 'lucid-dream', 'lunar-eclipse', 'macaroni-penguin',
     'magellanic-penguin', 'mammoth-tusk', 'maple-leaf-fall', 'maple-syrup', 'medieval-knights',
     'mediterranean-dreams', 'melting-glaciers', 'midnight-alchemy', 'midnight-train', 'midnight-elegance',
