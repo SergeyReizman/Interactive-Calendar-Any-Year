6b129c57b30b12a645ef24c32c279fd0bddb1e9e
SergeyReizman <sergeytlv1971@gmail.com>
2025-06-20 00:09:32 +0300

    555

--- calendar.js
+++ calendar.js
@@ -53,7 +53,7 @@ const applyTheme = (theme) => {
   body.classList.remove(
     'abandoned-castle', 'a-bright-future', 'a-brilliant-mind',
     'action-time', 'adelaide-skyline', 'a-desert-island', 'adriatic-sea', 'a-forward-move',
-    'a-good-idea', 'aloe-vera', 'a-lovely-day',
+    'a-good-idea', 'aloe-vera', 'alone-in-paradise', 'a-lovely-day',
     'alpha-centauri', 'alpine-landscape', 'amazing-opportunity', 'amazon-dolphin',
     'amazon-river', 'andromeda-galaxy', 'antarctic-ice',
     'antarctic-snowdrifts', 'arctic-glow', 'arctic-snowfall', 
