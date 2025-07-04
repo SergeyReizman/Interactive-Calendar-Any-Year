075a4f46fe595947e3137e800c6c657c00b101fd
SergeyReizman <sergeytlv1971@gmail.com>
2025-07-04 14:37:26 +0300

    c b

--- calendar.js
+++ calendar.js
@@ -75,7 +75,7 @@ const applyTheme = (theme) => {
     'camomile-flowers', 'canary-islands', 'can-do-attitude', 'candy-world',
     'cardinals-guards', 'caribbean-sea', 'celestial-objects', 'centaurus-constellation',
     'cherry-garden', 'cinnamon-street', 'city-of-dreams', 'city-of-night', 
-    'clean-code', 'clean-energy', 
+    'clean-code', 'clean-energy', 'coastal-beauty',
     'coastal-pacific', 'coconut-islands', 'coffee-for-leaders', 'color-symphony',
     'collaboration-idea', 'competitive-advantage',
     'continuous-growth', 'coral-reef', 'cosmic-nebula', 'cosmic-perspective',
