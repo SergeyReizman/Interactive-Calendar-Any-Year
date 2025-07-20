5904e6297fdb9c0cda43bc21415b1a8dc51b34dd
SergeyReizman <sergeytlv1971@gmail.com>
2025-07-20 22:41:53 +0300

    565654465464

--- calendar.js
+++ calendar.js
@@ -173,7 +173,7 @@ const applyTheme = (theme) => {
     'strategic-advantage', 'strategic-planning',
     'strawberry-moon', 'street-art-installation', 'strength-of-spirit', 'strong-foundation',
     'style-spectrum', 'success-key','success-motivation', 'success-story', 
-    'summer-breeze', 'sunset-magic', 'sweet-misty-morning',
+    'summer-breeze', 'summer-vibes', 'sunset-magic', 'sweet-misty-morning',
     'sword-of-fate', 'technological-future',
     'technological-might', 'tactical-advantage', 'tactical-command', 
     'the-art-of-visible', 'the-best-of-fashion', 'the-black-spruce', 
