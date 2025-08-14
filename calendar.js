c92b8ce787ca59161620df122571d9773330cac8
SergeyReizman <sergeytlv1971@gmail.com>
2025-08-14 21:07:25 +0300

    5645131

--- calendar.js
+++ calendar.js
@@ -74,7 +74,7 @@ const applyTheme = (theme) => {
     'black-orange', 'black-pear', 'black-pearl', 'black-style',
     'black-velvet', 'black-violet', 'blue-banana', 'blue-bird', 
     'blue-bliss', 'blue-frost', 'blue-gold',
-    'blue-ice', 'blue-mountains','blue-ocean',
+    'blue-ice', 'blue-island-waters', 'blue-mountains','blue-ocean',
     'blue-planet', 'blue-sky', 'blue-stars', 'blue-system',
     'blue-whale', 'blue-white', 'blue-window',
     'bold-vision', 'bora-bora', 'box-of-surprises', 'breaking-point', 
