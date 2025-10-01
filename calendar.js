9ee344576258dbdc1ee2bb4b7d41a1a0b291fe2a
SergeyReizman <sergeytlv1971@gmail.com>
2025-10-01 22:28:57 +0300

    123

--- calendar.js
+++ calendar.js
@@ -217,7 +217,7 @@ const applyTheme = (theme) => {
     'the-silver-sword', 'the-spirit-of-innovation', 'the-unstoppable-mindset', 'the-west-coast', 
     'the-world-of-nature', 'think-different', 'tide-of-dreams', 'tiger-design',
     'time-complexity', 'time-machine', 'time-to-chill', 'tokyo-at-night',
-    'too-beautiful-to-be-true', 'toronto-raptors',
+    'too-beautiful-to-be-true', 'toronto-raptors', 'tropical-horizon',
     'tropical-islands', 'tropical-jungle', 'tropical-nature', 'tropical-paradise',
     'tropical-sunset', 'turquoise-frontier', 'twilight-garden', 'underwater-life',
     'unexpected-layout', 'unforgettable-moments', 'unseen-wonders', 'untouched-paradise', 
